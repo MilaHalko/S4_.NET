dc7ef892e16165f0d82d4bd4e78762769f059910
Mila Halko <milagalko1@gmail.com>
2022-08-02 22:58:13 +0300

    Classes devided into files + report is done

---
+++ SudokuMementoProg/SudokuMementoProg/SudokuHistory.cs
@@ -0,0 +1,12 @@
+﻿namespace SudokuMementoProg
+{
+    internal class SudokuHistory
+	{
+		public Stack<SudokuMemento> history { get; private set; }
+		public SudokuHistory()
+		{
+			history = new Stack<SudokuMemento>();
+		}
+
+	}
+}
---
+++ SudokuMementoProg/SudokuMementoProg/SudokuMemento.cs
@@ -0,0 +1,18 @@
+﻿namespace SudokuMementoProg
+{
+	internal class SudokuMemento
+	{
+		public int[,] field { get; private set; }
+		public SudokuMemento(int[,] field)
+		{
+			this.field = new int[9, 9];
+			for (int i = 0; i < 9; i++)
+			{
+				for (int j = 0; j < 9; j++)
+				{
+					this.field[i, j] = field[i, j];
+				}
+			}
+		}
+	}
+}
